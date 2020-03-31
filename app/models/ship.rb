f58eb7da388ca8dfb09a91e9f7e87ff46269bb84
Roger Robertson Jr <rjrjr@pm.me>
2020-03-31 16:35:43 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+    @@all = []
+    attr_reader :name, :type, :booty
+
+    def initialize(name, type, booty)
+        @name = name
+        @type = type
+        @booty = booty
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
